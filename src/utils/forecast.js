3008cf6e4c8881a831b23893eebb92bd2791820d
Miriam Gertrudix <mgertrudix@quercus.local@itw104.quercus.local>
2021-07-09 10:59:21 +0200

    more info in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, 'It is currently ' + body.current.temperature + ' degress out.')   
+            callback(undefined, 'Right now it is '+body.current.weather_descriptions + ', and it is currently ' + body.current.temperature + ' degress out. With a ' + body.current.humidity +' % of humidity.')   
         }
     })
 }
